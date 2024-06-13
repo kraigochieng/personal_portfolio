17bc25b6d89dfb60cd8048ef40ddb370df22793f
kraigochieng <kraigochieng@gmail.com>
2024-06-13 22:35:11 +0300

    specified host and port

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import uvicorn
 if __name__ == "__main__":
     uvicorn.run(
         app="app.main:app",
-        port=8000,
+        host="0.0.0.0",
+        port=10000,
         reload=True
     )
