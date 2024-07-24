276baf390f3324b23a19ce3871a32652d213803e
kraigochieng <kraigochieng@gmail.com>
2024-07-24 11:28:28 +0300

    modified: mounted static folder to app

--- app/main.py
+++ app/main.py
@@ -1,14 +1,23 @@
+import os
+
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette.requests import Request
-import os
+
 app = FastAPI()
+
+# Set up static folder
+app.mount("/static", app=StaticFiles(directory="app/static"), name="static")
 
 # Set up Jinja2 templates
 current_dir = os.path.dirname(os.path.abspath(__file__))
 templates = Jinja2Templates(directory=os.path.join(current_dir, "templates"))
 
+
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "title": "Hello, FastAPI!"})
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "title": "Hello, FastAPI!"}
+    )
